eee0b61ec4c2c38d60a1985a0f581de5866a4330
AKHILBABY07 <akinbaby2000@gmail.com>
2023-10-08 14:21:51 +0530

    Banner complete

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl ='https://api.themoviedb.org/3';
-export const API_KEY =''
+export const API_KEY ="0fca9d7d905c4d4a1d8a8439190945a6";
+export const imageUrl ='https://image.tmdb.org/t/p/original'
