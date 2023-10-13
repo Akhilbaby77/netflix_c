c9e05c1fb0949d4b70e0324a770385062076ebac
Amarr7777 <ambadypradeepcoc@gmail.com>
2023-10-13 09:53:33 +0530

    API_key updated

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,23 @@ import {getFirestore} from "@firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB9eZx7P2u8xp7Quo1TMI65fuucuTF5_Dk",
+//   authDomain: "netflix-email-authentication.firebaseapp.com",
+//   projectId: "netflix-email-authentication",
+//   storageBucket: "netflix-email-authentication.appspot.com",
+//   messagingSenderId: "961885446535",
+//   appId: "1:961885446535:web:50032e77ea4cce3036f322"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB9eZx7P2u8xp7Quo1TMI65fuucuTF5_Dk",
-  authDomain: "netflix-email-authentication.firebaseapp.com",
-  projectId: "netflix-email-authentication",
-  storageBucket: "netflix-email-authentication.appspot.com",
-  messagingSenderId: "961885446535",
-  appId: "1:961885446535:web:50032e77ea4cce3036f322"
+  apiKey: "AIzaSyA-f5NsCAD_85ZzOvJHcmKYwVN5HVl2vbQ",
+  authDomain: "netflixclone-5c756.firebaseapp.com",
+  projectId: "netflixclone-5c756",
+  storageBucket: "netflixclone-5c756.appspot.com",
+  messagingSenderId: "385742952252",
+  appId: "1:385742952252:web:cddbad6ce2f3bdad0ab039"
 };
 
 // Initialize Firebase
